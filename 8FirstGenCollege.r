4060642ec01e483671e28bb329b38aa7a228ad88
loqoman <darwin.s.clark@gmail.com>
2021-03-16 12:41:55 -0700

    :sparkles: Finishing assignment 8

--- 8FirstGenCollege.r
+++ 8FirstGenCollege.r
@@ -21,8 +21,42 @@ firstGenGPASD = sd(firstGenRows$GPA)
 notFirstGenGPAMean = mean(notFirstGenRows$GPA)
 notFirstGenGPASD = sd(notFirstGenRows$GPA)
 
-
-# Work for part b
+# === Work for part (b) === 
 partBRegression = lm(rawData$GPA~rawData$FirstGen)
+# For fun lines
 summary(partBRegression)
+plot(partBRegression)
 
+plot(residuals(partBRegression), main="Residual Plot")
+qqnorm(rawData$GPA)
+
+# --- Calculating Sb ---
+# Finding Sxx
+avgFirstGen = mean(rawData$FirstGen)
+Sxx = sum((rawData$FirstGen-avgFirstGen)^2)
+
+# Finding Se
+sumresids = sum(residuals(partBRegression)^2)
+Se = sqrt(sumresids / (219-2))
+Sb = Se / Sxx
+
+# === Work for part (c) ===
+# Logistic regression
+partCRegression = glm(rawData$FirstGen~rawData$GPA, family=binomial("logit"))
+# Predicting first generational status from GPA
+summary(partCRegression)
+
+# --- Calculating Sb ---
+# Finding Sxx
+avgGPA = mean(rawData$GPA)
+Sxx = sum((rawData$GPA-avgGPA)^2)
+
+# Finding Se
+sumresids = sum(residuals(partCRegression)^2)
+Se = sqrt(sumresids / (219-2))
+
+Sb = Se / Sxx
+
+plot(residuals(partCRegression), main="Residual Plot")
+
+qqnorm(rawData$FirstGen)
