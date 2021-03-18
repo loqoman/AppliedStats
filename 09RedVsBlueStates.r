5b2251972881873da27ecbc563191bfa3716d11c
loqoman <darwin.s.clark@gmail.com>
2021-03-18 10:02:25 -0700

    :sparkles: Part (1) and (2) for assignment 9

--- 09RedVsBlueStates.r
+++ 09RedVsBlueStates.r
@@ -11,19 +11,29 @@ rawData = read_excel("Programming/AppliedStats/Election08.xls")
 # The first 195 lines are NOT first gen
 # The remaining 24 lines ARE first gen
 
-# Work for part A
+# === Work for part 1 ===
 # Fitting a logistic regression by income per capita
-OneARegression = 
+OneARegression = glm(rawData$ObamaWin~rawData$Income, family=binomial("logit"))
 summary(OneARegression)
 
 # Fitting a logistic regression by percent HS
-OneBRegression = 
+OneBRegression = glm(rawData$ObamaWin~rawData$HS, family=binomial("logit"))
 summary(OneBRegression)
 
 # Fitting a logistic regression by percent BS
-OneCRegression = 
+OneCRegression = glm(rawData$ObamaWin~rawData$BA, family=binomial("logit"))
 summary(OneCRegression)
 
 # Fitting a logistic regression by percent party delta
-OneDRegression = 
+OneDRegression = glm(rawData$ObamaWin~rawData$Dem.Rep, family=binomial("logit"))
 summary(OneDRegression)
+
+
+
+
+# === Work for part 2 ===
+
+# === Work for part 5 ===
+# Creating a col for income/1000
+rawData$ScaledIncome = rawData$Income/1000
+
