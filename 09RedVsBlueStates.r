1fff81a49fa66403fe737bdae97d2c2bb0d620bd
loqoman <darwin.s.clark@gmail.com>
2021-03-24 16:11:38 -0700

    :rocket: Finished all code for assignment 9

--- 09RedVsBlueStates.r
+++ 09RedVsBlueStates.r
@@ -13,7 +13,7 @@ rawData = read_excel("Programming/AppliedStats/Election08.xls")
 
 # === Work for part 1 ===
 # Fitting a logistic regression by income per capita
-OneARegression = glm(rawData$ObamaWin~rawData$Income, family=binomial("logit"))
+OneARegression = glm(ObamaWin~Income, family=binomial("logit"), data=rawData)
 summary(OneARegression)
 
 # Fitting a logistic regression by percent HS
@@ -31,10 +31,44 @@ summary(OneDRegression)
 
 
 # === Work for part 2 ===
+# === Work for part 4 ===
+# Getting our input list
+inputIncomes = data.frame(Income=seq(28845, 61052)) # Creates every income within the data set 28845, 61052
+
+# data.frame(Income=inputIncomes)
+outPutProbability = predict(OneARegression, newdata=inputIncomes, type="response")
+
+outPutOdds = outPutProbability / (1-outPutProbability)
+allOddsRatio = c()
+
+for (index in seq(1, 32207)) {
+  print(outPutOdds[as.integer(index)] / outPutOdds[as.integer(index)+1])
+  allOddsRatio = c(allOddsRatio, (outPutOdds[as.integer(index+1)] / outPutOdds[as.integer(index)]))
+  #append(allOddsRatio, )
+}
+
 
 # === Work for part 5 ===
 # Creating a col for income/1000
 rawData$ScaledIncome = rawData$Income/1000
 
-Part5Regression = glm(rawData$ObamaWin~rawData$ScaledIncome, family=binomial("logit"))
+Part5Regression = glm(ObamaWin~ScaledIncome, family=binomial("logit"), data=rawData)
 summary(Part5Regression)
+
+# === Work for part 6 ===
+# Getting our input list
+inputIncomes = data.frame(ScaledIncome=seq(28.845, 61.052, by=0.001)) # Creates every income within the data set 28845, 61052
+
+# data.frame(Income=inputIncomes)
+outPutProbability = predict(Part5Regression, newdata=inputIncomes, type="response")
+
+outPutOdds = outPutProbability / (1-outPutProbability)
+allOddsRatio = c()
+
+for (index in seq(1, 32207)) {
+  print(outPutOdds[as.integer(index)] / outPutOdds[as.integer(index)+1])
+  allOddsRatio = c(allOddsRatio, (outPutOdds[as.integer(index+1)] / outPutOdds[as.integer(index)]))
+  #append(allOddsRatio, )
+}
+
+sd(allOddsRatio)
