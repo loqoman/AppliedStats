09a6aeac2ec5b840294067216cf3d3b320dc53d5
loqoman <darwin.s.clark@gmail.com>
2021-04-02 17:16:25 -0700

    :sparkles: Done with all r code for part 13

--- 10Titanic.r
+++ 10Titanic.r
@@ -5,8 +5,8 @@ library(ggplot2) # Used to make graphics
 library(readxl)  # Excel reading lib
 library(plyr)    # Used to count occurancs of a categorical data
 
-library(extrafont)
-loadfonts(device = "win")
+# library(extrafont)
+# loadfonts(device = "win")
 
 rawData = read_excel("Programming/AppliedStats/titanic.xls")
 
@@ -39,5 +39,90 @@ mean(thirdClass$Fare)
 
 # === Part 3 ===
 ggplot(rawData, aes(rawData$Survived, fill = rawData$Sex)) +
-  geom_bar(position="fill") +
-  theme_minimal() + xlab() + ylab()
+  geom_bar(position="fill", width = 0.3) + # Changing to "fill" will cause normalize all bars to be the same height
+                                            # Changing to "stack" will cause the height of the bars to be proportional to count
+  theme_minimal() + 
+  xlab(label="Groupings of Passengers (left = died, right = lived)") + 
+  ylab(label="Percentage of the Respective Passenger Population") +
+  ggtitle("Stacked bar Chart Comparing Proportion of Passenger Survival by Gender") +
+  labs(fill="Gender")
+
+ggplot(rawData, aes(rawData$Survived, fill = rawData$Sex)) +
+  geom_bar(position="stack", width = 0.3) + # Changing to "fill" will cause normalize all bars to be the same height
+  # Changing to "stack" will cause the height of the bars to be proportional to count
+  theme_minimal() + 
+  xlab(label="Groupings of Passengers (left = died, right = lived)") + 
+  ylab(label="Number of Respective Passenger ") +
+  ggtitle("Stacked bar Chart Comparing Count of Passenger Survival by Gender") +
+  labs(fill="Gender")
+
+# === Part 5 ===
+
+ggplot(rawData, aes(factor(rawData$Survived), rawData$Age)) + 
+  geom_boxplot() +
+  coord_flip() + 
+  xlab(label="Grouping of Passenger (top = survived, bottom = died)") + 
+  ylab(label="Age of Passenger") +
+  ggtitle("Horizontal Box Plots Comparing age Distribution of Living and Dead Passengers") 
+
+# === Part 5 ===
+class(rawData$Survived)
+class(rawData$Sex)
+class(rawData$Pclass)
+
+ggplot(rawData, aes(rawData$Survived, fill = as.character(rawData$Pclass))) +
+  geom_bar(position="stack", width = 0.3) + # Changing to "fill" will cause normalize all bars to be the same height
+  # Changing to "stack" will cause the height of the bars to be proportional to count
+  theme_minimal() + 
+  xlab(label="Groupings of Passengers (left = died, right = lived)") + 
+  ylab(label="Number of respective Passenger ") +
+  ggtitle("Stacked bar Chart Comparing Count of Passenger Survivial by Ticket Class") +
+  labs(fill="Ticket Class")
+
+
+# === Part 7 ===
+survivalByAge = glm(Survived~Age, family=binomial("logit"), data=rawData)
+summary(survivalByAge)
+
+# === Part 9 ===
+survivalBySex = glm(Survived~Sex, family=binomial("logit"), data=rawData)
+summary(survivalBySex)
+
+# === Part 9 ===
+survivalByClass = glm(Survived~Pclass, family=binomial("logit"), data=rawData)
+summary(survivalByClass)
+
+# === Part 13 ===
+# lm16 = summary(lm(mydata$volume ~ (mydata$minwidth + mydata$maxwidth + mydata$material)))
+
+# With no Interactive Terms
+all3Variables1 = glm(Survived~(Pclass+Sex+Age), family=binomial("logit"), data=rawData)
+
+# With 2 interacting terms
+all3Variables2 = glm(Survived~(Pclass*Sex+Age), family=binomial("logit"), data=rawData)
+all3Variables3 = glm(Survived~(Pclass+Sex*Age), family=binomial("logit"), data=rawData)
+all3Variables4 = glm(Survived~(Pclass*Age+Sex), family=binomial("logit"), data=rawData)
+
+# With all interacting terms
+all3Variables4 = glm(Survived~(Pclass*Age*Sex), family=binomial("logit"), data=rawData)
+
+
+summary(all3Variables4)
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
